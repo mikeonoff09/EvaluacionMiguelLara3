38f7009a14ce772df44db2454c54b536238eceb8
Miguel Lara <miguel.larasj@gmail.com>
2023-10-29 18:05:46 -0500

    initial commit

---
+++ src/main/java/com/mikels/test3/evaluacion/EvaluacionApplication.java
@@ -0,0 +1,13 @@
+package com.mikels.test3.evaluacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluacionApplication.class, args);
+	}
+
+}
